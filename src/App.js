6258db821abe31f97ab1653b94aae0fbea2e4ee9
Nainik Mehta <nainik.m@crestinfosystems.net>
2021-12-28 12:23:08 +0530

    React chrome extension to generate random words And their meaning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Vocab } from "./vocalList";
 function App() {
+  const randomNum = Math.floor(Math.random() * Math.floor(Vocab.length));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{Vocab[randomNum].FIELD1}</h1>
+      <h3>{Vocab[randomNum].FIELD2}</h3>
     </div>
   );
 }
